3636d06437b397af998a686a0c8189f13526f93b
student <student@stepschool.lan>
2023-10-21 14:24:17 +0300

    lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -4,17 +4,50 @@ class Car:
         self.speed = speed
         self.model = model
         self.weight = weight
-        self.passangers = []
+        self.passagers = []
 
-    def add_passanger(self, passanger):
-        self.passangers.append(passanger)
+    def add_passager(self, passager):
+        self.passagers.append(passager)
+
+    def show_passagers(self):
+        for i in self.passagers:
+            print(i)
+
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, happiness, gender):
         self.name = name
         self.age = age
+        self.happiness = happiness
+        self.money = 100000
+        self.gender = gender
+        self.job = None
+        self.house = None
 
-bob = Person('Bob', 23)
+bob = Person('Bob', 23, 100, 100000)
+jack = Person('Jack', 24, 44)
+tom = Person('Tom', 19)
 car = Car(360, 'Mercedes', 2000)
-car.add_passanger(bob)
-print(car.passangers)
+car.add_passager(bob)
+car.add_passager(jack)
+car.add_passager(tom)
+car.show_passagers()
+print(car.passagers)
+
+
+def buy_car(self, car):
+    if self.money >= car.price:
+        self.car = car
+    else:
+        print('not enough money')
+
+class job:
+    def __init__(self, salary, name):
+        self.salary = salary
+        self.name = name
+
+class home:
+    def __init__(self, area):
+        self.area = area
+        self.people = []
+        self
